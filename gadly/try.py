433a82e274e839ac932f8df379910d308f390735
mosesalcantara <115634291+mosesalcantara@users.noreply.github.com>
2022-12-15 18:23:05 +0800

    Delete try.py

--- gadly/try.py
+++
@@ -1,8 +0,0 @@
-
-from nltk.tokenize import sent_tokenize, word_tokenize
-import nltk.corpus
-
-text = word_tokenize("And now for something completely different")
-c =nltk.pos_tag(text)
-for a in c:
-    print (a)
