0a00b4be6193def7272713756aa6ba4e32423f87
mosesalcantara <115634291+mosesalcantara@users.noreply.github.com>
2022-12-23 09:33:30 +0800

    jquery ajax

--- gadly/main/urls.py
+++ gadly/main/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
 
 app_name = 'main'
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.sign, name='sign'),
-    path('sign_in/', views.sign_in, name='sign_in'),
-    path('sign_up/', views.sign_up, name='sign_up'),
-    path('reset/', views.reset, name='reset'),
-    path('post_reset/', views.post_reset, name='post_reset'),
-    path('home/', views.home, name='home'),
-    path('paraphrase/', views.paraphrase, name='paraphrase'),
-    path('logout/', views.logout, name="logout"),
-    path('test2/', views.test2, name="test2"),
+    path('sign_in/', views.sign_in),
+    path('sign_up/', views.sign_up),
+    path('reset/', views.reset),
+    path('post_reset/', views.post_reset),
+    path('home/', views.home),
+    path('paraphrase_text/', views.paraphrase_text),
+    path('logout/', views.logout),
 ]
--- gadly/main/views.py
+++ gadly/main/views.py
@@ -1,11 +1,9 @@
 import pyrebase
 
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from json import dumps
-from .forms import TextForm
 from .main import Main
 
 
@@ -24,6 +22,9 @@ firebase=pyrebase.initialize_app(config)
 authe=firebase.auth()
 database=firebase.database()
 
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
 def sign(request):
     return render(request,"main/sign.html")
 
@@ -33,102 +34,39 @@ def home(request):
         #return render(request,"main/home.html")
         return render(request,"main/test.html")
     else:
-        return redirect('http://127.0.0.1:8000/main/')
+        return redirect('/main')
 
-
-def paraphrase(request):
-    if ('login' in request.session):
-        words=''
-        input_text=''
-        output_text=''
-        filtered_list=''
-        replacement_words=''
-        synonym_list=''
-
-        form=TextForm(request.POST or None)
+def paraphrase_text(request):
+    input_text=''
+    words=''
+    output_text=''
+    filtered_list=''
+    replacement_words=''
+    synonym_list=''
+    
+    if is_ajax(request=request):
+        input_text = request.POST['input_text']
+        obj = Main()
+        words, output_text, filtered_list, replacement_words, synonym_list = obj.main(input_text)
         
-        if form.is_valid():
-            obj = Main()
-            input_text = form.cleaned_data.get("input_text")
-            words, output_text, filtered_list, replacement_words, synonym_list = obj.main(input_text)
-            
         json_data={
-            'words' : words,
             'input_text': input_text, 
+            'words' : words,
             'output_text': output_text,
             'filtered_list' : filtered_list, 
             'synonym_list' : synonym_list,  
             'replacement_words' : replacement_words
         }
         
-        dataJSON=dumps(json_data)
-        
-        context={
-            'words' : words,
-            'input_text': input_text, 
-            'output_text': output_text, 
-            'filtered_list' : filtered_list, 
-            'synonym_list' : synonym_list, 
-            'replacement_words' : replacement_words,
-            'dataJSON' : dataJSON
-        }
-
-        data={
+        db_data={
             'Sensitive Words' : filtered_list,
             'Replacement Words' : replacement_words
         }
-                
-        database.child("Detected Words").push(data)
-        #return render(request, 'main/home.html', context)
-        return render(request, 'main/test.html', context)
-    else:
-        return redirect('http://127.0.0.1:8000/main/')
-    
-def test2(request):
-    words=''
-    input_text=''
-    output_text=''
-    filtered_list=''
-    replacement_words=''
-    synonym_list=''
-
-    form=TextForm(request.POST or None)
-    
-    if form.is_valid():
-        obj = Main()
-        input_text = form.cleaned_data.get("input_text")
-        words, output_text, filtered_list, replacement_words, synonym_list = obj.main(input_text)
         
-    json_data={
-        'words' : words,
-        'input_text': input_text, 
-        'output_text': output_text,
-        'filtered_list' : filtered_list, 
-        'synonym_list' : synonym_list,  
-        'replacement_words' : replacement_words
-    }
+        database.child("Detected Words").push(db_data)
+                
+        return JsonResponse(json_data)
     
-    dataJSON=dumps(json_data)
-    
-    context={
-        'words' : words,
-        'input_text': input_text, 
-        'output_text': output_text, 
-        'filtered_list' : filtered_list, 
-        'synonym_list' : synonym_list, 
-        'replacement_words' : replacement_words,
-        'dataJSON' : dataJSON
-    }
-
-    data={
-        'Sensitive Words' : filtered_list,
-        'Replacement Words' : replacement_words
-    }
-            
-    database.child("Detected Words").push(data)
-    #return render(request, 'main/home.html', context)
-    return render(request, 'main/test2.html', context)
-
 @csrf_exempt
 def sign_in(request):
     email=request.POST.get('email')
@@ -139,12 +77,12 @@ def sign_in(request):
     except:
         message="Invalid Credentials!!Please Check your Data"
         # return render(request,"Login.html",{"message":message})
-        return redirect('http://127.0.0.1:8000/main/')
+        return redirect('/main/')
     session_id=user['idToken']
     request.session['uid']=str(session_id)
     request.session['login'] = True
     # return render(request,"main/paraphrase.html",{"email":email})
-    return redirect('http://127.0.0.1:8000/main/home')
+    return redirect('/main/home')
 
 
 @csrf_exempt
@@ -159,8 +97,8 @@ def sign_up(request):
         idtoken = request.session['uid']
         print(uid)
      except:
-        return redirect('http://127.0.0.1:8000/main/')
-     return redirect('http://127.0.0.1:8000/main/')
+        return redirect('/main')
+     return redirect('/main')
  
  
 def logout(request):
@@ -169,7 +107,7 @@ def logout(request):
         del request.session['login']
     except:
         pass
-    return redirect('http://127.0.0.1:8000/main/')
+    return redirect('/main')
  
  
 def reset(request):
